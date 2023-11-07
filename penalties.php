cd2b1c7e7c983e77ee4909943adce5a2e7bbbb8d
hyegunn <92244602+hyegunn@users.noreply.github.com>
2023-11-07 15:38:39 +0800

    Add files via upload

---
+++ penalties.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Penalty for School</title>
+    <link rel="stylesheet" type="text/css" href="penalties-css.css">
+</head>
+<body>
+    <header>
+        <h1>Penalty for School</h1>
+    </header>
+    <div class="container">
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSYufv9uNAP0IMqa3hUtVjgS-T5J4rnGDKghcB51QEVCF46A-M-ucT9qAq5TvRbURsw1ngGUDnxGn4g/pubhtml?gid=915921041&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+    </div>
+</body>
+</html>
